1a93adfb2766b6689716a456d3d495f10f543daa
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2022-12-04 22:05:03 +0300

    EcommerceApp

---
+++ client/src/pages/ProductList.jsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+import { useLocation } from "react-router";
+import { useState } from "react";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>{cat}</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filtrelenmiş Ürünler:</FilterText>
+          <Select name="color" onChange={handleFilters}>
+            <Option disabled>Renk</Option>
+            <Option>Beyaz</Option>
+            <Option>Siyah</Option>
+            <Option>Kırmızı</Option>
+            <Option>Mavi</Option>
+            <Option>Sarı</Option>
+            <Option>Yeşil</Option>
+          </Select>
+          <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Ürün Sıralanması:</FilterText>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">En Yeniler</Option>
+            <Option value="asc">Fiyata Göre Artan</Option>
+            <Option value="desc">Fiyata Göre Azalan</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products cat={cat} filters={filters} sort={sort} />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
